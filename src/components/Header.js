6166d149f34599f29b877a713924fb2dec4c950e
andy <a5706617@gmail.com>
2022-09-24 01:12:00 +0800

    test

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return (
+        <header className = "header d-flex row flex column">
+            <h2 class="text-white pt-5 pb-2">
+                HAPE PRIME
+                </h2>
+                <p
+                    class="text-light opacity-3 text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-400 text-opacity-80">
+                    8K NEXT-GENERATION, HIGH FASHION HAPES
+                </p>
+                <div class="flex mt-6 justify-center">
+                    <hr class="hr-header-line rounded" />
+                </div>
+        </header>
+    );
+}
